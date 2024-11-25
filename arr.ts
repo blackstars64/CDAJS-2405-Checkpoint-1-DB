5bfe8dda16cd3118a148a538e7dc090e58e14ad6
blackstars <nelson.64@live.fr>
2024-11-25 12:00:35 +0100

    ## Part two

--- arr.ts
+++ arr.ts
@@ -1,16 +1,42 @@
 class Arr {
-  value: string[] = [];
+	value: string[] = [];
 
-  push(el: string): number {
-    const array = new Array(this.value.length + 1);
+	push(el: string): number {
+		const array = new Array(this.value.length + 1);
 
-    for (let i = 0; i < this.value.length; i++) {
-      array[i] = this.value[i];
-    }
-    array[this.value.length] = el;
-    this.value = array;
-    return this.value.length;
-  }
+		for (let i = 0; i < this.value.length; i++) {
+			array[i] = this.value[i];
+		}
+		array[this.value.length] = el;
+		this.value = array;
+		return this.value.length;
+	}
+
+	pop(): string | undefined {
+		if (this.value.length === 0) {
+			return undefined;
+		}
+		const array = new Array(this.value.length - 1);
+		for (let i = 0; i < array.length; i++) {
+			array[i] = this.value[i];
+		}
+		const removedElement = this.value[this.value.length - 1];
+		this.value = array;
+		return removedElement;
+	}
+
+	shift(): string | undefined {
+		if (this.value.length === 0) {
+			return undefined;
+		}
+		const array = new Array(this.value.length - 1);
+		for (let i = 1; i < this.value.length; i++) {
+			array[i - 1] = this.value[i];
+		}
+		const removedElement = this.value[0];
+		this.value = array;
+		return removedElement;
+	}
 }
 
 export default Arr;
--- test/array.test.ts
+++ test/array.test.ts
@@ -1,17 +1,55 @@
 import Arr from "../arr";
 
 describe("Array", () => {
-  it("should create an array", () => {
-    const newArray = new Arr();
+	it("should create an array", () => {
+		const newArray = new Arr();
+		expect(newArray.value).toHaveLength(0);
+	});
 
-    expect(newArray.value).toHaveLength(0);
-  });
+	it("should add an element to the array", () => {
+		const newArray = new Arr();
+		const result = newArray.push("a");
 
-  it("should had an element in the array", () => {
-    const newArray = new Arr();
-    const result = newArray.push("a");
+		expect(newArray.value).toHaveLength(1);
+		expect(result).toEqual(1);
+		expect(newArray.value[0]).toEqual("a");
+	});
 
-    expect(newArray.value).toHaveLength(1);
-    expect(result).toEqual(1);
-  });
+	it("should remove the last element from the array", () => {
+		const newArray = new Arr();
+		newArray.push("a");
+		newArray.push("b");
+		newArray.push("c");
+
+		const result = newArray.pop();
+
+		expect(result).toEqual("c");
+		expect(newArray.value).toHaveLength(2);
+		expect(newArray.value).toEqual(["a", "b"]);
+
+		newArray.pop();
+		newArray.pop();
+
+		expect(newArray.value).toHaveLength(0);
+		expect(newArray.pop()).toBeUndefined();
+	});
+
+	it("should remove the first element from the array", () => {
+		const newArray = new Arr();
+		newArray.push("a");
+		newArray.push("b");
+		newArray.push("c");
+
+		const result = newArray.shift();
+
+		expect(result).toEqual("a");
+		expect(newArray.value).toHaveLength(2);
+		expect(newArray.value).toEqual(["b", "c"]);
+
+		newArray.shift();
+		newArray.shift();
+
+		expect(newArray.value).toHaveLength(0);
+		expect(newArray.shift()).toBeUndefined();
+	});
 });
